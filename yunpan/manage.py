61fd91c004883fb193c75601a7e32fdcc1cdcd48
zzhouzhee <zzhouzhee@yahoo.com>
2014-03-12 16:56:52 +0800

    create a project yunpan

--- yunpan/manage.py
+++ yunpan/manage.py
@@ -1,13 +1,10 @@
-__author__ = 'Administrator'
-
 #!/usr/bin/env python
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "file_test.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yunpan.settings")
 
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-
