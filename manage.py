b1401bce19f3537efb45db1b2133643d661caed9
zzhouzhee <zzhouzhee@yahoo.com>
2014-03-12 16:06:26 +0800

    add manage.py

---
+++ manage.py
@@ -0,0 +1,13 @@
+__author__ = 'Administrator'
+
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "file_test.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+    
